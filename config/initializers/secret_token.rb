a604084ddadf6e72705fa7c693a6fbbcf0c1ec4b
Matt MacDonald <matt.macdonald@gmail.com>
2013-01-12 14:20:45 -0500

    Initial commit of shell Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MuniMeeting::Application.config.secret_token = '802e9d34da69aae21201e1eab9fcff4f60c9b08f6ba263ab059f5b56113b3c87218c0c79088aa5883c560906b1ea1d538cd4c41fadd35897ec0deea31891c7bc'
